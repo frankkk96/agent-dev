4924f4344554d1e401443bb068201689a43ff9fb
langtang1996 <langtang1996@gmail.com>
2025-06-20 09:12:20 +0800

    update

--- agent_dev/agents/__init__.py
+++ agent_dev/agents/__init__.py
@@ -1,8 +1,8 @@
-from .base import Post, ChatStatus, AgentMetadata
+from .base import Post, ChatStatus, AgentMetadata, ModelProvider
 from .rss_agent import RSSAgent
 from .image_gen_agent import ImageGenAgent
 from .simple_chat_agent import SimpleChatAgent
 from .tool_call_agent import ToolCallAgent
 
-__all__ = ["Post", "ChatStatus", "AgentMetadata",
+__all__ = ["Post", "ChatStatus", "AgentMetadata", "ModelProvider",
            "RSSAgent", "ImageGenAgent", "SimpleChatAgent", "ToolCallAgent"]
